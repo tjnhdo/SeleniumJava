9862847e927bb065df2c1283ff7222d115335bbe
tinhdo <tinhdo@tinhs-mbp>
2022-09-12 21:51:01 +0700

    Update POM file

---
+++ JavaTestNG/src/test/java/NewTest.java
@@ -0,0 +1,36 @@
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class NewTest {
+	
+	@Test
+	public void TestAddition() {
+		int a = 5;
+		int b = 4;
+		int actual = a+b;
+		int expected = 9;
+		
+		Assert.assertEquals(actual, expected);	
+	}
+	
+	@Test
+	public void TestSubstraction() {
+		int a = 5;
+		int b = 4;
+		int actual = a-b;
+		int expected = 1;
+		
+		Assert.assertEquals(actual, expected);	
+	}
+	
+	@Test
+	public void TesMultiplication() {
+		int a = 5;
+		int b = 4;
+		int actual = a*b;
+		int expected = 20;
+		
+		Assert.assertEquals(actual, expected);	
+	}
+	
+}
